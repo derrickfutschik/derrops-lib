b9c1bddd9ad0e5a6619b1042f5a282e5bdcb362e
Derrick Futschik <derrickfutschik@gmail.com>
2026-01-01 14:45:35 +1100

    generate the openapi.json

---
+++ apps/slaops-cloud/src/openapi.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+import { AppModule } from './app.module';
+
+async function generateOpenApi() {
+
+    console.log(`Creating Application (without database connection)`);
+
+    const app = await NestFactory.create(AppModule.forRoot({ skipDatabase: true }), {
+        logger: false
+    });
+
+    const config = new DocumentBuilder()
+        .setTitle('SLAOps API')
+        .setDescription('Internal API')
+        .setVersion('1.0.0')
+        .addBearerAuth()
+        .build();
+
+    console.log(`Generating OpenAPI Document`);
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    const outputPath = join(process.cwd(), 'dist', 'openapi.json');
+    writeFileSync(outputPath, JSON.stringify(document, null, 2));
+
+    await app.close();
+
+    console.log(`OpenAPI written to ${outputPath}`);
+}
+
+generateOpenApi();
