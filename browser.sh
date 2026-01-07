15fa7d29466b2cb9d77b74bbf6ca52831891c3cf
Derrick Futschik <derrickfutschik@gmail.com>
2026-01-08 04:50:09 +1100

    removed opening tab from quick start

---
+++ browser.sh
@@ -0,0 +1,20 @@
+# Open OpenSearch Dashboards in Safari
+open -a Safari http://192.168.7.224:5601
+
+# Open the Portal
+open -a Safari http://localhost:8080
+
+# Open the Docs
+open -a Safari http://localhost:8081
+
+# Open the SLAOPs Cloud
+open -a Safari http://localhost:3001/api
+
+# Open Syncthing Server
+open -a Safarihttp://192.168.7.224:8384/#
+
+# Open Syncthing Local Host
+open -a Safari http://localhost:8384/#
+
+# Open PG Admin
+open -a Safari http://192.168.7.224:5050
--- quickstart.sh
+++ quickstart.sh
@@ -1,24 +1,3 @@
-# Open OpenSearch Dashboards in Safari
-open -a Safari http://192.168.7.224:5601
-
-# Open the Portal
-open -a Safari http://localhost:8080
-
-# Open the Docs
-open -a Safari http://localhost:8081
-
-# Open the SLAOPs Cloud
-open -a Safari http://localhost:3001/api
-
-# Open Syncthing Server
-open -a Safarihttp://192.168.7.224:8384/#
-
-# Open Syncthing Local Host
-open -a Safari http://localhost:8384/#
-
-# Open PG Admin
-open -a Safari http://192.168.7.224:5050
-
 # Open the Tmuxinator session
 tmuxinator start -p .tmuxinator.yml
 
