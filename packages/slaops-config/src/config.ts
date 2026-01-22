d370b52faeabc48f509f0792fd40451c6081ba59
Derrick Futschik <derrickfutschik@gmail.com>
2026-01-22 22:10:29 +1100

    config update

--- packages/slaops-config/src/config.ts
+++ packages/slaops-config/src/config.ts
@@ -1,21 +1,59 @@
 import { configFromEnv } from "./from-env";
+import { AppConfigEnv } from "./schema";
 
-export const makeConfig = (env: NodeJS.ProcessEnv = process.env) => {
-    const cfg = configFromEnv(env);
+export const makeConfig = (cfg: AppConfigEnv = configFromEnv(process.env)) => {
+    // const cfg = configFromEnv(env);
+
+
     return {
+
+        // Application properties, properties regarding the actual application
         "app.port": cfg.PORT,
         "app.env": cfg.NODE_ENV,
-        "app.name": "SLAOps",
+        "app.name": cfg.APP_NAME,
         "app.version": cfg.APP_VERSION,
-        "app.title": "SLAOps Cloud API",
-        "app.description": "SLAOps is a platform for managing and monitoring SLA metrics",
-        "app.author": "Derrops",
-        "app.license": "MIT",
-        "app.url": "https://slaops.com",
-        "app.email": "derrickfutschik@hotmail.com",
-        "app.phone": "+1234567890",
-        "app.address": "123 Main St, Anytown, USA",
+
+        // Open API properties
+        "openapi.version": cfg.APP_VERSION,
+        "openapi.title": "SLAOps Cloud API",
+        "openapi.description": "SLAOps is a platform for managing and monitoring SLA metrics of SAAS applications",
+        "openapi.author": "Derrops",
+        "openapi.license": "MIT",
+        "openapi.url": "https://slaops.com",
+        "openapi.email": "derrickfutschik@hotmail.com",
+        "openapi.phone": "+1234567890",
+        "openapi.address": "123 Main St, Anytown, USA",
+
+        // Database properties
+        "db.username": cfg.DB_USERNAME,
+        "db.password": cfg.DB_PASSWORD,
+        "db.host": cfg.DB_HOST,
+        "db.port": cfg.DB_PORT,
+        "db.database": cfg.DB_DATABASE,
+        "db.ssl": cfg.DB_SSL,
+        "db.logging": cfg.DB_LOGGING,
+
+        //opensearch properties
+
+        /** The region opensearch is deployed in  */
+        "opensearch.region": cfg.AWS_REGION,
+
+        /** The endpoint to access opensearch */
+        "opensearch.endpoint": cfg.OPENSEARCH_ENDPOINT,
+
+        /** The index prefix */
+        "opensearch.index.prefix": `${cfg.APP_NAME}-`.toLowerCase(),
+
+        /** The suffix for the index */
+        "opensearch.index.suffix": `-${cfg.NODE_ENV}`.toLowerCase(),
+
+        /** Find the name of an index */
+        "opensearch.index": (entity: string) => `${cfg.APP_NAME}-${entity}-${cfg.NODE_ENV}`.toLowerCase(),
+
+
     }
 }
 
 export type AppConfig = ReturnType<typeof makeConfig>;
+
+export const config = makeConfig();
--- packages/slaops-config/src/schema.ts
+++ packages/slaops-config/src/schema.ts
@@ -1,20 +1,32 @@
 import { z } from "zod";
 
 
-// TODO - do not have the schema on the environment variables, but rather on the AppConfig interface.
+// TODO - there should be no default variables and rather manage this through environment variables somehwo
+
 export const ConfigSchema = z.object({
+
   NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
   PORT: z.coerce.number().default(3000),
 
-  DB_HOST: z.string().min(1),
-  DB_PORT: z.coerce.number().default(5432),
-  DB_USERNAME: z.string().min(1),
-  DB_PASSWORD: z.string().min(1),
   DB_NAME: z.string().min(1),
-  DB_SSL: z.string().optional(),
-  DB_LOGGING: z.string().optional(),
+  DB_SSL: z.string().optional().default("false"),
+  DB_LOGGING: z.string().optional().default("false"),
+  DB_HOST: z.string().min(1).default("192.168.7.224"),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_DATABASE: z.string().min(1).default("slaops"),
+  DB_SCHEMA: z.string().min(1).default("public"),
+  DB_USERNAME: z.string().min(1).default("postgres"),
+  DB_PASSWORD: z.string().min(1).default("postgres"),
 
+  APP_NAME: z.string().min(1).default("SLAOps"),
   APP_VERSION: z.string().min(1).default("0.0.1"), // should come from package.json
+
+  AWS_REGION: z.string().min(1).default("ap-southeast-2"),
+
+  OPENSEARCH_INDEX_PREFIX: z.string().min(1).default("slaops-"),
+  OPENSEARCH_INDEX_NAME: z.string().min(1).default("opensearch-index"),
+  OPENSEARCH_ENDPOINT: z.string().min(1).default("http://192.168.7.224:9200"),
+
 });
 
 export type AppConfigEnv = z.infer<typeof ConfigSchema>;
