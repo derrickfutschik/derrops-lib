8b7629c6cc104c69bb6ec1de3b8a97929d873212
Derrick Futschik <derrickfutschik@gmail.com>
2026-04-07 00:29:41 +1000

    API URL Override

--- apps/slaops-portal/src/config.ts
+++ apps/slaops-portal/src/config.ts
@@ -1,1 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || process.env.VITE_API_BASE_URL
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL_OVERRIDE ||
+  process.env.VITE_API_BASE_URL_OVERRIDE ||
+  import.meta.env.VITE_API_BASE_URL ||
+  process.env.VITE_API_BASE_URL
