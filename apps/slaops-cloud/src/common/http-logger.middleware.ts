fff91a1ab2fc86a9c15ae80f85e4e63568141a5e
Derrick Futschik <derrickfutschik@gmail.com>
2026-04-20 08:25:01 +1000

    logging

---
+++ apps/slaops-cloud/src/common/http-logger.middleware.ts
@@ -0,0 +1,28 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
+import { Request, Response, NextFunction } from 'express'
+
+@Injectable()
+export class HttpLoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP')
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl } = req
+    const start = Date.now()
+
+    res.on('finish', () => {
+      const { statusCode } = res
+      const ms = Date.now() - start
+      const message = `${method} ${originalUrl} ${statusCode} ${ms}ms`
+
+      if (statusCode >= 500) {
+        this.logger.error(message)
+      } else if (statusCode >= 400) {
+        this.logger.warn(message)
+      } else {
+        this.logger.log(message)
+      }
+    })
+
+    next()
+  }
+}
